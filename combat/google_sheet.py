d94ed15c2ee14029e85b81ac28df4af621f631b9
dthayer <dthayer@bwschools.net>
2026-06-23 02:09:56 -0400

    Update google sheets connection

--- combat/google_sheet.py
+++ combat/google_sheet.py
@@ -1,16 +1,16 @@
+import streamlit as st
 import gspread
-from google.oauth2.credentials import Credentials
+from google.oauth2.service_account import Credentials
 
 SCOPES = ["https://www.googleapis.com/auth/spreadsheets"]
 
-SHEET_URL = "https://docs.google.com/spreadsheets/d/1o0lFaYlVnSvIuXfe7jdI1_LQSCVwBUosto_mhjz_d5U/edit?gid=0#gid=0"
+SHEET_URL = st.secrets["google"]["sheet_url"]
 
 def get_sheet():
-    creds = Credentials.from_authorized_user_file(
-        "token.json",
-        SCOPES
+    creds = Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=SCOPES
     )
 
     client = gspread.authorize(creds)
-
     return client.open_by_url(SHEET_URL).sheet1
